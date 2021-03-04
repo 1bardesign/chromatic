ff5f0077676c5b3343b9d2ca9ca1d902c47c9b3f
Max Cahill <1bardesign@gmail.com>
2021-03-04 21:07:49 +1100

    [added] image.pad

--- image.lua
+++ image.lua
@@ -6,4 +6,27 @@ local image = {}
 
 --todo: import image_utils
 
+--pad an image with a given colour
+function image.pad(image, amount, r, g, b, a)
+	--default
+	r = r or 0
+	g = g or r
+	b = b or r
+	a = a or 1
+	--
+	local w, h = image:getDimensions()
+	local out = love.image.newImageData(w + amount * 2, h + amount * 2, image:getFormat())
+	out:mapPixel(function(x, y)
+		if
+			x < amount or x >= amount + w
+			or y < amount or y >= amount + h
+		then
+			return r, g, b, a
+		else
+			return image:getPixel(x - amount, y - amount)
+		end
+	end)
+	return out
+end
+
 return image
